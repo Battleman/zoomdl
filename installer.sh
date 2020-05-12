8cc1cf6e3d7d95202dc6919182b79e34b55b59e4
Olivier Cloux <olivier.cloux@epfl.ch>
2020-05-12 14:45:54 +0200

    installer for easier use

---
+++ installer.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+PYTHON="/usr/bin/env python"
+EXEC="zoomdl"
+ZIP="zoomdl.zip"
+ZIP_FOLDER="zip"
+LOCALBIN="/usr/local/bin/"
+SRC="zoom_dl"
+compile(){
+	mkdir -p $ZIP_FOLDER #create folder to zip
+	cp -r $SRC $ZIP_FOLDER #copy all python files
+	mv $ZIP_FOLDER/$SRC/__main__.py $ZIP_FOLDER #put main above src
+	pip install -r requirements.txt --target $ZIP_FOLDER
+	rm -r $ZIP_FOLDER/*.dist-info
+	python3 -m  zipapp -p "interpreter" -o $EXEC
+
+	rm -rf $ZIP_FOLDER
+}
+
+install(){
+	compile
+	sudo mv $EXEC $LOCALBIN
+}
+
+if [[ $# -ne 1 ]]
+then
+		echo "Please input one command such as 'compile' or 'install'"
+		exit 1
+else
+	"$@"
+fi
