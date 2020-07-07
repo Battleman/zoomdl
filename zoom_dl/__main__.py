95ed416b6525f7653d31d6cc89ff6aedf1b5953c
Olivier Cloux <olivier.cloux@epfl.ch>
2020-07-07 19:15:45 +0200

    Adapting init and main for a more intuitive layout

--- zoom_dl/__main__.py
+++ zoom_dl/__main__.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python3
-from __future__ import unicode_literals
+# coding: utf-8
+"""Define the main module.
+
+This deals with either being called as a frozen or dynamic package.
+"""
 
 import sys
-
-def check_positive(value):
-    """Ensure a given value is a positive integer"""
-    ivalue = int(value)
-    if ivalue < 0:
-        raise argparse.ArgumentTypeError("%s is an invalid positive int value" % value)
-    return ivalue
 
 if __package__ is None and not hasattr(sys, 'frozen'):
     # direct call of __main__.py
@@ -18,40 +15,6 @@ if __package__ is None and not hasattr(sys, 'frozen'):
     print(os.path.dirname(path))
 
 import zoom_dl
-import requests
-import argparse
 
 if __name__ == '__main__':
-    PARSER = argparse.ArgumentParser(
-        description="Utility to download zoom videos",
-        prog="zoomdl",
-        formatter_class=lambda prog: argparse.HelpFormatter(prog,
-                                                            max_help_position=10,
-                                                            width=200)
-    )
-
-    PARSER.add_argument("-u", "--url",
-                        help=("Enter the url of the video to download. "
-                              "Looks like 'zoom.us/rec/play/...'"),
-                        type=str,
-                        required=True,
-                        metavar="zoom url")
-    PARSER.add_argument("-f", "--filename",
-                        help=("The name of the output video file without "
-                              "extension. Default to the filename according "
-                              "to Zoom. Extension is automatic."),
-                        metavar="filename")
-    PARSER.add_argument("-p", "--password",
-                        help="Password of the video (if any)",
-                        metavar="password")
-    PARSER.add_argument("-c", "--count-clips",
-                        help=("If multiple clips, how many to download. "
-                              "1 = only current URL (default). "
-                              "0 = all of them. "
-                              "Other positive integer, count of clips to "
-                              "download, starting from the current one"),
-                        metavar="Count of clips to download",
-                        type=check_positive,
-                        default=1)
-    args = PARSER.parse_args()
-    zoom_dl.zoomdl(args)
+    zoom_dl.main()
---
+++ zoom_dl/utils.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+# coding: utf-8
+"""Define useful methods to be used globally."""
+
+import argparse
+
+
+def _check_positive(value):
+    """Ensure a given value is a positive integer."""
+    int_value = int(value)
+    if int_value < 0:
+        raise argparse.ArgumentTypeError(
+            "%s is an invalid positive int value" % value)
+    return int_value
+
+
+def parseOpts():
+    """Parse command line arguments.
+
+    Returns:
+        argparse.Namespace: Namespace of the parsed arguments.
+    """
+    PARSER = argparse.ArgumentParser(
+        description="Utility to download zoom videos",
+        prog="zoomdl",
+        formatter_class=(lambda prog:
+                         argparse.HelpFormatter(prog,
+                                                max_help_position=10,
+                                                width=200)
+                         ))
+
+    PARSER.add_argument("-u", "--url",
+                        help=("Enter the url of the video to download. "
+                              "Looks like 'zoom.us/rec/play/...'"),
+                        type=str,
+                        required=True,
+                        metavar="url")
+    PARSER.add_argument("-f", "--filename",
+                        help=("The name of the output video file without "
+                              "extension. Default to the filename according "
+                              "to Zoom. Extension is automatic."),
+                        metavar="filename")
+    PARSER.add_argument("-p", "--password",
+                        help="Password of the video (if any)",
+                        metavar="password")
+    PARSER.add_argument("-c", "--count-clips",
+                        help=("If multiple clips, how many to download. "
+                              "1 = only current URL (default). "
+                              "0 = all of them. "
+                              "Other positive integer, count of clips to "
+                              "download, starting from the current one"),
+                        metavar="Count",
+                        type=_check_positive,
+                        default=1)
+    PARSER.add_argument("-v", "--log-level",
+                        help=("Chose the level of verbosity. 0=debug, 1=info "
+                              "(default), 2=warning 3=Error, 4=Critical, "
+                              "5=Quiet (nothing printed)"),
+                        metavar="level",
+                        type=int,
+                        default=1)
+    PARSER.add_argument("-b", "--browser",
+                        help=("Indicate the browser from which retrieve the "
+                              "cookies, to bypass passwords and reCAPTCHA. "
+                              "Currently supported: Firefox and Chrome."),
+                        metavar="Browser")
+    return PARSER.parse_args()
