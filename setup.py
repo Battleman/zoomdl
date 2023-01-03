131aaf945a799b9433942287835fda09027d7b3e
Niklas Bogensperger <niklasbogensperger@users.noreply.github.com>
2023-01-03 22:56:15 +0100

    Consistent double-quote use in setup.py

--- setup.py
+++ setup.py
@@ -6,22 +6,22 @@ import os
 # README as long_description
 current_directory = os.path.dirname(os.path.abspath(__file__))
 try:
-    with open(os.path.join(current_directory, 'README.md'), encoding='utf-8') as f:
+    with open(os.path.join(current_directory, "README.md"), encoding="utf-8") as f:
         long_description = f.read()
 except Exception:
     long_description = ""
 
 setup(
     name="zoomdl",
-    version=version.__version__,
+    version="2023.01.03",
     description="Download Zoom recorded meetings easily",
     url="https://github.com/Battleman/zoomdl",
     author="Olivier Cloux",
     license="MIT",
-    packages=['zoomdl'],
-    package_data={'zoomdl': ['zoomdl/*']},
+    packages=["zoomdl"],
+    package_data={"zoomdl": ["zoomdl/*"]},
     include_package_data=True,
-    install_requires=['demjson3~=3.0.6', 'requests~=2.28.1', 'tqdm~=4.64.1'],
+    install_requires=["demjson3~=3.0.6", "requests~=2.28.1", "tqdm~=4.64.1"],
     entry_points={
         "console_scripts": [
             "zoomdl=zoomdl:main",
