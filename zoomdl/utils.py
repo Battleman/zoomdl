fea6fb1a499f90e1467d9a4b4f60ab2d29cc1e57
Niklas Bogensperger <niklasbogensperger@users.noreply.github.com>
2023-01-03 23:08:46 +0100

    Delete version info from helptext again
    Does not work on binaries built with devscripts.sh

--- zoomdl/utils.py
+++ zoomdl/utils.py
@@ -101,7 +101,7 @@ def parseOpts(args: List[str]):
         argparse.Namespace: Namespace of the parsed arguments.
     """
     PARSER = argparse.ArgumentParser(
-        description="Utility to download zoom videos. Version: " + version("zoomdl"),
+        description="Utility to download zoom videos.",
         prog="zoomdl",
         formatter_class=(lambda prog:
                          argparse.HelpFormatter(prog,
